e4a341494f966fca10e12bde80c8066fc4aafa59
oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2025-08-01 14:22:06 +0800

    Update landrun-install.sh

--- files/scripts/landrun-install.sh
+++ files/scripts/landrun-install.sh
@@ -3,7 +3,7 @@
 # Tell this script to exit if there are any errors.
 set -oue pipefail
 
-git clone https://github.com/zouuup/landrun.git /tmp/
-cd /tmp/landrun
+git clone https://github.com/zouuup/landrun.git /tmp/landrun-build/
+cd /tmp/landrun-build/landrun/
 go build -o landrun cmd/landrun/main.go
 cp landrun /usr/bin/
