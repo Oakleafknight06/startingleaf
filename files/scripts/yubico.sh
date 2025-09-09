e0ef49ec3079b42b2912ccb297b1fb25ebc4f04a
Oakleafknight06 <65838823+Oakleafknight06@users.noreply.github.com>
2025-09-09 09:53:47 -0400

    Yubico auth install script update

--- files/scripts/yubico.sh
+++ files/scripts/yubico.sh
@@ -7,13 +7,18 @@ set -oue pipefail
 mkdir /tmp/yubico
 wget https://developers.yubico.com/yubioath-flutter/Releases/yubico-authenticator-latest-linux.tar.gz /tmp/yubico
 tar -xzvf /tmp/yubico/yubico-authenticator-latest-linux.tar.gz
+
 # Move the .desktop file to the right place
 mv /tmp/yubico/yubico-authenticator-latest-linux/linux_support/com.yubico.yubioath.desktop /usr/share/applications/
+
 # Not sure if the icon will work this way
-mv /tmp/yubico/yubico-authenticator-latest-linux/linux_support/com.yubico.yubioath.png /usr/share/icons
+# mv /tmp/yubico/yubico-authenticator-latest-linux/linux_support/com.yubico.yubioath.png /usr/share/icons
+
 # Move the executable and other stuff to the proper directories
-mv /tmp/yubico/yubico-authenticator-latest-linux/authenticator /usr/bin/
-mv /tmp/yubico/yubico-authenticator-latest-linux/data # ??
-mv /tmp/yubico/yubico-authenticator-latest-linux/helper # not sure
-mv /tmp/yubico/yubico-authenticator-latest-linux/lib # to /usr/lib?
-# "authenticator" is the binary, "data" "helper" "lib" directories reqired. Not sure where they go though. Can one have folders in /usr/bin?
+mkdir /usr/bin/yubico-authenticator
+mv /tmp/yubico/yubico-authenticator-latest-linux/authenticator /usr/bin/yubico-authenticator
+mv /tmp/yubico/yubico-authenticator-latest-linux/data /usr/bin/yubico-authenticator
+mv /tmp/yubico/yubico-authenticator-latest-linux/helper /usr/bin/yubico-authenticator
+mv /tmp/yubico/yubico-authenticator-latest-linux/lib /usr/bin/yubico-authenticator
+
+# "authenticator" is the binary, "data" "helper" "lib" directories required. Not sure where they go though. Can one have folders in /usr/bin?
